e24e466094d110ef8691a5e7414c6e01e354ae5a
David Craven <david@craven.ch>
2021-12-27 15:32:56 +0100

    Fix ios build.

--- packages/tlfs_ios/ios/Classes/TlfsPlugin.m
+++
@@ -1,7 +0,0 @@
-#import "TlfsPlugin.h"
-
-@implementation TlfsPlugin
-+ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftTlfsPlugin registerWithRegistrar:registrar];
-}
-@end
